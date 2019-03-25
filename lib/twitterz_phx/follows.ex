e98a2be5c7d9966ab35433c754b325e297d4d1b7
indrajit <eendroroy@gmail.com>
2019-03-25 23:29:48 +0600

    update follow un_follow query

--- lib/twitterz_phx/follows.ex
+++ lib/twitterz_phx/follows.ex
@@ -6,15 +6,20 @@ defmodule TwitterZPhx.Follows do
   alias TwitterZPhx.Users.User
 
   def add_follow(%User{} = user, %User{} = follow) do
-    %Follow{}
-    |> Follow.changeset(%{user_id: user.id, follow_id: follow.id})
-    |> Repo.insert()
+    case Repo.get_by(Follow, user_id: user.id, follow_id: follow.id) do
+      nil ->
+        %Follow{}
+        |> Follow.changeset(%{user_id: user.id, follow_id: follow.id})
+        |> Repo.insert()
+      _ -> ""
+    end
   end
 
   def delete_follow(%User{} = user, %User{} = follow) do
-    follow = Repo.get_by(Follow, user_id: user.id, follow_id: follow.id)
-    if follow != nil do
-      Repo.delete(follow)
+    case Repo.get_by(Follow, user_id: user.id, follow_id: follow.id) do
+      nil -> ""
+      follow -> Repo.delete(follow)
+      _ -> ""
     end
   end
 end
